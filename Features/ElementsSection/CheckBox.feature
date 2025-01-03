485341887080cf7f6e6aca78868081a2c256618a
Csaba Tóth <tothxcsaba@gmail.com>
2025-01-03 18:04:01 +0100

    feat: added beta of CheckBox feature and steps

---
+++ Features/ElementsSection/CheckBox.feature
@@ -0,0 +1,28 @@
+﻿Feature: Test functions under Elements/Check Box
+
+@elements @checkbox
+    Scenario: Home node selected
+        Given I am on the homepage
+        When I click on Elements section
+        And I click on Check Box
+        #And I select "Home"
+        #Then the "You have selected :" section appears
+        #And the following selections are listed
+        #| result     |
+        #| home       |
+        #| desktop    |
+        #| notes      |
+        #| commands   |
+        #| documents  |
+        #| workspace  |
+        #| react      |
+        #| angular    |
+        #| veu        |
+        #| office     |
+        #| public     |
+        #| private    |
+        #| classified |
+        #| general    |
+        #| downloads  |
+        #| wordFile   |
+        #| excelFile  |
---
+++ StepDefinitions/ElementsSection/CheckBoxStepDefinitions.cs
@@ -0,0 +1,46 @@
+﻿using DemoQA.Contexts;
+using DemoQA.Drivers;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using Reqnroll;
+
+[Binding]
+public class CheckBoxStepDefinitions
+{
+
+    private readonly IWebDriver? _driver;
+    private readonly ElementsContext _context;
+
+    public CheckBoxStepDefinitions(ElementsContext context)
+    {
+        _driver = WebDriverSetup.GetDriver();
+        _context = context;
+    }
+
+    [When("I click on Check Box")]
+    public void WhenIClickOnCheckBox()
+    {
+        IWebElement checkBoxButton = _driver!.FindElement(By.XPath("//span[text()='Check Box']"));
+
+        checkBoxButton.Click();
+    }
+
+    [When("I select {string}")]
+    public void WhenISelect(string home)
+    {
+        throw new PendingStepException();
+    }
+
+    [Then("the {string} section appears")]
+    public void ThenTheSectionAppears(string p0)
+    {
+        throw new PendingStepException();
+    }
+
+    [Then("the following selections are listed")]
+    public void ThenTheFollowingSelectionsAreListed(DataTable dataTable)
+    {
+        throw new PendingStepException();
+    }
+}
