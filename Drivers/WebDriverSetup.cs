4914cd6f517403cc42bd41fa2696a68ed1f46709
Csaba Tóth <tothxcsaba@gmail.com>
2025-01-03 18:05:38 +0100

    refactor: updated WebDriverSetup and Hooks so shared features and files use the same webdriver easily

--- Drivers/WebDriverSetup.cs
+++ Drivers/WebDriverSetup.cs
@@ -7,40 +7,51 @@ using WebDriverManager.DriverConfigs.Impl;
 
 namespace DemoQA.Drivers;
 
-public static class WebDriverSetup
+public  class WebDriverSetup
 {
     // Define browser type
-    private const string Browser = "firefox";
+    private const string Browser = "chrome";
     
     private static IWebDriver? _driver;
     
+    public static void SetupDriver()
+    {
+        if (_driver == null)
+        {
+            switch (Browser.ToLower())
+            {
+                case "chrome":
+                    new DriverManager().SetUpDriver(new ChromeConfig());
+                    var chromeOptions = new ChromeOptions();
+                    //chromeOptions.AddArgument("--start-maximized");
+                    _driver = new ChromeDriver(chromeOptions);
+                    break;
+
+                case "firefox":
+                    new DriverManager().SetUpDriver(new FirefoxConfig());
+                    var fireFoxOptions = new FirefoxOptions();
+                    //fireFoxOptions.AddArgument("--start-maximized");
+                    _driver = new FirefoxDriver(fireFoxOptions);
+                    break;
+
+                case "edge":
+                    new DriverManager().SetUpDriver(new EdgeConfig());
+                    var edgeOptions = new EdgeOptions();
+                    //edgeOptions.AddArgument("start-maximized");
+                    _driver = new EdgeDriver(edgeOptions);
+                    break;
+
+                default:
+                    throw new ArgumentException($"Browser '{Browser}' is not supported. Please use 'chrome', 'firefox', or 'edge'");
+            }
+        }
+    }
+
     public static IWebDriver GetDriver()
     {
-        switch (Browser.ToLower())
+        if (_driver == null)
         {
-            case "chrome":
-                new DriverManager().SetUpDriver(new ChromeConfig());
-                var chromeOptions = new ChromeOptions();
-                chromeOptions.AddArgument("--start-maximized");
-                _driver = new ChromeDriver(chromeOptions);
-                break;
-
-            case "firefox":
-                new DriverManager().SetUpDriver(new FirefoxConfig());
-                var fireFoxOptions = new FirefoxOptions();
-                fireFoxOptions.AddArgument("--start-maximized");
-                _driver = new FirefoxDriver(fireFoxOptions);
-                break;
-
-            case "edge":
-                new DriverManager().SetUpDriver(new EdgeConfig());
-                var edgeOptions = new EdgeOptions();
-                edgeOptions.AddArgument("start-maximized");
-                _driver = new EdgeDriver(edgeOptions);
-                break;
-
-            default:
-                throw new ArgumentException($"Browser '{Browser}' is not supported. Please use 'chrome', 'firefox', or 'edge'");
+            throw new InvalidOperationException("WebDriver has not been initialized.");
         }
 
         return _driver;
--- Hooks/Hooks.cs
+++ Hooks/Hooks.cs
@@ -1,11 +1,26 @@
 ﻿using DemoQA.Drivers;
+using OpenQA.Selenium;
 using Reqnroll;
+using WebDriverManager;
 
 namespace DemoQA.Hooks
 {
     [Binding]
     public class Hooks
     {
+        private readonly WebDriverSetup _webDriverSetup;
+
+        public Hooks(WebDriverSetup webDriverSetup)
+        {
+            _webDriverSetup = webDriverSetup;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            WebDriverSetup.SetupDriver();
+        }
+
         [AfterScenario]
         public static void AfterTestRun()
         {
